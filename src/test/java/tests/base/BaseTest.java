6f65d9f2340fb68f7888e920f034f0e15ada9409
Kostya <lutcevolov@icloud.com>
2021-09-07 10:40:43 +0300

    qwe

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -38,7 +38,7 @@ public abstract class BaseTest {
             options.addArguments("--no-sandbox");
             options.addArguments("--headless");
             options.addArguments("start-maximized");
-            driver = new RemoteWebDriver(new URL("http://127.0.0.1:8081/"),options);
+            driver = new RemoteWebDriver(new URL("http://172.18.0.1:4444/"),options);
            /* driver = new ChromeDriver(options);*/
             driver.manage().window().maximize();
         } else if (browser.equals("firefox")) {
